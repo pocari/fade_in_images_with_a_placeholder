6b53bbeacfcd6320e0ef6b70d6d4a1954c93f87e
pocari <caffelattenonsugar@gmail.com>
2019-12-14 15:12:38 +0900

    initial commit

---
+++ android/app/src/main/kotlin/com/example/fade_in_images_with_a_placeholder/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.fade_in_images_with_a_placeholder
+
+import androidx.annotation.NonNull;
+import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+        GeneratedPluginRegistrant.registerWith(flutterEngine);
+    }
+}
